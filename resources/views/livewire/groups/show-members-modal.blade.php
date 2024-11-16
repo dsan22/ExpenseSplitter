9e1371dcb7a752f3870759dad2cf8fbfc1c363a5
dsan22 <jankovicdusan00@gmail.com>
2024-11-16 01:24:06 +0100

    added table to modal for members

--- resources/views/livewire/groups/show-members-modal.blade.php
+++ resources/views/livewire/groups/show-members-modal.blade.php
@@ -12,7 +12,7 @@ new class extends Component {
     }
     public function mount($group){
         $this->group=$group;
-        $this->users=$group->users;
+        $this->users = $group->users->except(auth()->id());
     }
 }; ?>
 
@@ -21,6 +21,18 @@ new class extends Component {
         <x-button.circle lg sky icon="user-group"  wire:click="openModal"/>
     </div>
     <x-modal.card title="Members" blur wire:model="modalOpen">
-        <livewire:groups.add-members-modal :group="$group"/>
+        <div class="flex justify-end px-8 space-x-3">
+            <x-button negative label="Leave"/>
+            <livewire:groups.add-members-modal :group="$group" />
+        </div>
+       
+        <div class="bg-gray-50 mt-5 p-6">
+            @foreach ($users as $user)
+                <div class="flex items-center justify-between py-4 border-b border-gray-200">
+                    <div class="text-gray-700 font-medium">{{ $user->name }}</div>
+                    <x-button.circle sm negative icon="user-remove" wire:click="removeFromGroup" />
+                </div>
+            @endforeach
+        </div>
     </x-modal.card>
 </div>
